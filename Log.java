59e6c28cb577d88ac21be0a11e1dbd1a37aafa60
shubham7489 <en21ca501179@medicaps.ac.in>
2023-12-02 22:57:44 +0530

    first commit

---
+++ Log.java
@@ -0,0 +1,6 @@
+public class Log {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+    
+}
---
+++ Tata.java
@@ -0,0 +1,9 @@
+
+
+void main(){
+    String str="ram";
+    String str1=STR. "raja \{str}";
+    System.out.println(str1);
+    System.out.println( str1.equals("raja ram"));   // true
+
+}
